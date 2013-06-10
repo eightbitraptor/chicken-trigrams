437b3159b36c5108b92c4a5b042ab8e37e358faa
Matt House <matt@eightbitraptor.com>
2013-06-10 07:54:57 +0100

    getting closer

--- trigrams.scm
+++ trigrams.scm
@@ -12,16 +12,34 @@
     (split-into-triplets (append result (list (take-right input 3)))
                          (drop-right input 1))))
 
-(define (triplet-split tokens)
-    (reverse (split-into-triplets '() tokens)))
+(define (triplet-split sentances)
+    (reverse (split-into-triplets '() (tokenize sentances))))
 
 (define (token-group grp)
-  (list (list (first grp) (second grp))
+  (cons (list (first grp) (second grp))
         (list (last grp))))
 
 (define (trigram-key-structure triplets)
   (map token-group triplets))
 
-(define (convert-to-trigram input)
-  (trigram-key-structure input))
+(define (alist-value-by-ref-or-default key alist)
+  (let ((value (assoc key alist list=)))
+    (if value
+      (cdr value)
+      '())))
+
+(define (convert-word-groups-to-trigram alist word-groups)
+  (if (null? word-groups)
+    alist
+    (let ((key (car (token-group (car word-groups))))
+          (tokens (token-group (car word-groups))))
+      (convert-word-groups-to-trigram 
+        (cons 
+          (cons key (flatten (list (alist-value-by-ref-or-default 
+                                     (car tokens) 
+                                     alist)
+                             (cdr tokens))))
+          alist)
+        (cdr word-groups)))))
+       
 
--- trigrams_spec.scm
+++ trigrams_spec.scm
@@ -1,12 +1,9 @@
 (load "trigrams.scm")
  
-(define (test-input)
+(define test-input
   "The cat sat on the mat on the cat")
 
-(define (test-input-as-list)
-  '(The cat sat on the mat on the cat))
-
-(define (test-table-output)
+(define test-table-output
   '((The cat sat)
     (cat sat on)
     (sat on the)
@@ -15,36 +12,30 @@
     (mat on the)
     (on the cat)))
 
-(define (grouped-trigrams)
-  '(((The cat) (sat))
-    ((cat sat) (on))
-    ((sat on) (the))
-    ((on the) (mat))
-    ((the mat) (the))
-    ((mat on) (cat))
-    ((on the) (mat cat))))
-
-(define (unique-token-data)
-  '((The cat) (cat sat) (sat on) (on the) (the mat) (mat on)))
+(define grouped-trigrams
+  '(((The cat) sat)
+    ((cat sat) on)
+    ((sat on) the)
+    ((on the) mat)
+    ((the mat) the)
+    ((mat on) cat)
+    ((on the) mat cat)))
 
 (require-extension missbehave missbehave-matchers)
 
-(describe "Splitting a string into tokens"
-  (it "returns a list of tokens"
-    (expect (tokenize (test-input))
-            (be (test-input-as-list)))))
-
-(describe "grouping the tokens"
-  (it "groups 3 tokens into key/value pairs"
-    (expect (token-group '(The cat sat))
-            (be '((The cat) (sat))))))
-
 (describe "splitting the string into token groups"
   (it "has 3 tokens per group"
-    (expect (triplet-split (test-input-as-list))
-            (be (test-table-output))))
+    (expect (triplet-split test-input)
+            (be test-table-output))))
 
-  (it "converts the triplets into an associative list of trigrams"
-    (expect (convert-to-trigram (test-table-output))
-            (be (grouped-trigrams)))))
+(describe "grouping the test table into a table of trigrams"
+  (it "makes an association list"
+    (expect (convert-word-groups-to-trigram '() test-table-output)
+            (be a list)))
+  (it "correctly groups multiple words"
+    (expect (assoc 
+              '(on the) 
+              (convert-word-groups-to-trigram '() test-table-output))
+            (be '((on the) mat cat)))))
 
+(print (convert-word-groups-to-trigram '() test-table-output))
